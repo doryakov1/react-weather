8c0b9f1bdd0a4296759e867c0eeb60a0c4f7866b
dor yakov <betshean@walla.com>
2022-08-24 10:15:30 +0300

    first commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,8 @@ import './Header.css'
 export default function Header() {
   return (
     <div className='header'>
-        <Link className='link' to='/'>Home</Link>
-        <Link className='link' to='/favorit'>Favorit</Link>
+        <Link className='link' to='/react-weather'>Home</Link>
+        <Link className='link' to='/react-weather/favorit'>Favorit</Link>
     </div>
   )
 }
